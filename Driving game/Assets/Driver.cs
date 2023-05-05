e132ec3ea957322ed1836b547c807bc4696e75a2
Silvia <silviadmgz@gmail.com>
2023-05-05 21:26:43 +0200

    Added a worl and several mecanics

---
+++ Driving game/Assets/Driver.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 1;
+    [SerializeField] float moveSpeed = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.fixedDeltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime;
+
+        transform.Rotate(0,0, -steerAmount);
+        transform.Translate(0, moveAmount,0);
+
+    }
+}
---
+++ Driving game/Assets/FollowCamera.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] GameObject thingToFollow;
+
+
+    // The camera position must be the same as the car
+    void LateUpdate()
+    {
+        transform.position = thingToFollow.transform.position + new Vector3 (0,0,-10);
+    }
+}
