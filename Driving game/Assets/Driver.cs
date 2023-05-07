d2521c790e5260ead5510016b5e8179c3c8567dc
Silvia <silviadmgz@gmail.com>
2023-05-07 21:47:19 +0200

    Lecture finished

--- Driving game/Assets/Driver.cs
+++ Driving game/Assets/Driver.cs
@@ -6,11 +6,33 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 1;
     [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float slowSpeed = 0.62f;
+    [SerializeField] float boostSpeed = 5f;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Boost Speed")
+        {
+            moveSpeed = boostSpeed;
+        }
+
+        if(other.tag == "Slow Speed")
+        {
+            moveSpeed = slowSpeed;
+        }
     }
 
     // Update is called once per frame
