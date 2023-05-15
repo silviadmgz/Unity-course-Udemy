bcbe82206c025ffec608c99d58881e79231d24c0
Silvia <silviadmgz@gmail.com>
2023-05-15 14:27:11 +0200

    Finished

--- Snowboarder Game/Assets/Scrips/CrashDetector.cs
+++ Snowboarder Game/Assets/Scrips/CrashDetector.cs
@@ -7,11 +7,17 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float crashDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    bool playCrashSFX = true;
+    
     void OnTriggerEnter2D(Collider2D other) 
     {
-        if(other.tag == "Ground")
+        if(other.tag == "Ground" && playCrashSFX)
         {
+            FindObjectOfType<PlayerController>().DisableControls();
             crashEffect.Play();
+            playCrashSFX = false;
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("CrashReload", crashDelay);
         }
     }
