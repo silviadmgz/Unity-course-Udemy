b9576cf6c8be9e433e84e3b444a95653bafa447e
Silvia <silviadmgz@gmail.com>
2023-08-28 14:27:29 +0200

    Coin pickup sound

--- TileVania/Assets/Script/CoinPickup.cs
+++ TileVania/Assets/Script/CoinPickup.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     public int coinsPickedUp;
+    [SerializeField] AudioClip coinPickupSFX;
+    AudioSource coinSound;
 
     void OnTriggerEnter2D(Collider2D other) 
     {
@@ -18,8 +20,11 @@ public class CoinPickup : MonoBehaviour
     void PickUpCoin()
     {
         coinsPickedUp++;
+        coinSound = GameObject.Find("SFX AudioSource").GetComponent<AudioSource>();
+        coinSound.PlayOneShot(coinPickupSFX);
         Destroy(gameObject);
         Debug.Log(coinsPickedUp);
     }
 
+    
 }
