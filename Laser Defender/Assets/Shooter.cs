aa48cd9d48b736dd1be2814ce70cf415d993ebb8
Silvia <silviadmgz@gmail.com>
2023-09-11 15:26:00 +0200

    Player shooter
    Script
    Instantiating bullets

---
+++ Laser Defender/Assets/Shooter.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] float projectileSpeed = 10;
+    [SerializeField] float projectileLifetime = 5f;
+    [SerializeField] float firingRate = 0.2f;
+
+    public bool isFiring;
+
+    Coroutine firingCoroutine;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Fire();
+    }
+
+    void Fire()
+    {
+        if(isFiring && firingCoroutine == null)
+        {
+            firingCoroutine = StartCoroutine(FireContinuously());
+        } 
+        else if(!isFiring && firingCoroutine != null)
+        {
+            StopCoroutine(firingCoroutine);
+            firingCoroutine = null;
+        }
+    }
+
+    IEnumerator FireContinuously()
+    {
+        while (true)
+        {
+            GameObject instance = Instantiate(projectilePrefab, transform.position, quaternion.identity);
+            Rigidbody2D rigidbody2D = instance.GetComponent<Rigidbody2D>();
+            if (rigidbody2D != null)
+            {
+                rigidbody2D.velocity = transform.up * projectileSpeed;
+            }
+
+            Destroy(instance, projectileLifetime);
+            yield return new WaitForSeconds(firingRate);
+        }
+    }
+}
