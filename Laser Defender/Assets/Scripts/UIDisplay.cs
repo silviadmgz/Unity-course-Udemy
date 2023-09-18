a7ff39a77acae1cb3ae68e4361486d73a7a1c160
Silvia <silviadmgz@gmail.com>
2023-09-18 16:42:05 +0200

    UI Script

---
+++ Laser Defender/Assets/Scripts/UIDisplay.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UIDisplay : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
